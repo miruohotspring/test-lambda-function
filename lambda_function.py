dcef590e4ac0fc8d81e38cf2716f5e6abe2aee3d
miruohotspring <miruohotspring@gmail.com>
2021-03-29 04:21:26 +0900

    lambda handler

--- lambda_function.py
+++ lambda_function.py
@@ -1,1 +1,8 @@
-print("Hello World")
+import json
+
+def lambda_handler(event, context):
+    name = event['firstName'] + ' ' + event['lastName']
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda, ' + name)
+    }
